57b79af5a6f3ae40dd7a49aba8cdcfaa5e2d0e0e
Robbie Hill <rhill53@msudenver.edu>
2020-04-27 14:03:02 -0600

    all instruments added, questions fixed

--- OrchestraES.pl
+++ OrchestraES.pl
@@ -4,12 +4,12 @@
 % Nate Roberts and Robb Hill
 %
 
-instrument(oboe)					:- fact('is a woodwind'),	fact('has keys'),			fact('has a conical bore'),		fact('has a double reed').
-instrument(clarinet)				:- fact('is a woodwind'),	fact('has keys'),			fact('has a conical bore'),		fact('has a single reed').
-instrument(bassoo)					:- fact('is a woodwind'),	fact('has keys'),			fact('has no conical shape').
-instrument(flute)					:- fact('is a woodwind'),	fact('has keys'),			fact('is played to the side').
-instrument(saxaphone)				:- fact('is a woodwind'),	fact('is made of brass').
-instrument(piccolo)					:- fact('is a woodwind'),	fact('has no keys').
+instrument(oboe)					:- fact('is a woodwind instrument'),	fact('has keys'),			fact('has a conical bore'),		fact('has a double reed').
+instrument(clarinet)				:- fact('is a woodwind instrument'),	fact('has keys'),			fact('has a conical bore'),		fact('has a single reed').
+instrument(bassoo)					:- fact('is a woodwind instrument'),	fact('has keys'),			fact('has no conical shape').
+instrument(flute)					:- fact('is a woodwind instrument'),	fact('has keys'),			fact('is played to the side').
+instrument(saxaphone)				:- fact('is a woodwind instrument'),	fact('is made of brass').
+instrument(piccolo)					:- fact('is a woodwind instrument'),	fact('has no keys').
 % woodwind(contrabossoon) .
 % woodwind(b_flat_clarinet) .
 % woodwind(e_flat_clarinet) .
@@ -20,11 +20,11 @@ instrument(piccolo)					:- fact('is a woodwind'),	fact('has no keys').
 % woodwind(bass_oboe) .
 % woodwind(alto_flute) .
 
-instrument(horn)					:- fact('is brass'),	fact('is wide at end'),	fact('has valves'),	fact('is tubes bent in circles').
-instrument(trumpet)					:- fact('is brass'),	fact('is wide at end'), fact('has valves'),	fact('is tubes bent in ovals').
-instrument(trombone) 				:- fact('is brass'),	fact('is wide at end'),	fact('has slide').
-instrument(baritone) 				:- fact('is brass'),	fact('is wide at end'),	fact('has valves'), fact('end points up'), fact('is smaller than a tuba').
-instrument(tuba)					:- fact('is brass'),	fact('is wide at end'),	fact('has valves'), fact('end points up').
+instrument(horn)					:- fact('is a brass instrument'),	fact('is wide at end'),	fact('has valves'),	fact('is tubes bent in circles').
+instrument(trumpet)					:- fact('is a brass instrument'),	fact('is wide at end'), fact('has valves'),	fact('is tubes bent in ovals').
+instrument(trombone) 				:- fact('is a brass instrument'),	fact('is wide at end'),	fact('has slide').
+instrument(baritone) 				:- fact('is a brass instrument'),	fact('is wide at end'),	fact('has valves'), fact('end points up'), fact('is smaller than a tuba').
+instrument(tuba)					:- fact('is a brass instrument'),	fact('is wide at end'),	fact('has valves'), fact('end points up').
 % brass(wagner_tuba) .
 % brass(cornet) .
 % brass(piccolo_trumpet) .
@@ -36,17 +36,17 @@ instrument(tuba)					:- fact('is brass'),	fact('is wide at end'),	fact('has valv
 % brass(bariton_horn) .
 % brass(euphonium) .
 
-instrument(timpini) 			:- fact('is percussion'),	fact('has a strecched head'),		fact('has a metal body').
-instrument(snare_drum) 			:- fact('is percussion'),	fact('has a strecched head'),		fact('has springs attached to head').
-instrument(tamborine) 			:- fact('is percussion'),	fact('has a stretched head'),		fact('has paired metal discs all around edge').
-instrument(bass_drum) 			:- fact('is percussion'),	fact('has a strecched head'), 		fact('struck with felt covered stick').
-instrument(cymbals) 			:- fact('is percussion'),	fact('is metal'),					fact('are crashed together').
-instrument(triangle) 			:- fact('is percussion'),	fact('is metal'),					fact('has three sides').
-instrument(cow_bell)			:- fact('is percussion'),	fact('is metal'),					fact('has animal in name'),							fact('Christopher Walken needs more'),
-instrument(xylophone) 			:- fact('is percussion'),	fact('is metal'),					fact('has multiple sized bars').
-instrument(bells) 	 			:- fact('is percussion'),	fact('is metal'),					fact('has multiple sized tubes').
-instrument(wood_block) 			:- fact('is percussion'),	fact('is made entirely from wood').
-instrument(drum_kit) 			:- fact('is percussion'),	fact('is a set or kit'),			fact('has multiple percussion instruments'),.
+instrument(timpini) 			:- fact('is a percussion instrument'),	fact('has a strecched head'),		fact('has a metal body').
+instrument(snare_drum) 			:- fact('is a percussion instrument'),	fact('has a strecched head'),		fact('has springs attached to head').
+instrument(tamborine) 			:- fact('is a percussion instrument'),	fact('has a stretched head'),		fact('has paired metal discs all around edge').
+instrument(bass_drum) 			:- fact('is a percussion instrument'),	fact('has a strecched head'), 		fact('struck with felt covered stick').
+instrument(cymbals) 			:- fact('is a percussion instrument'),	fact('is metal'),					fact('are crashed together').
+instrument(triangle) 			:- fact('is a percussion instrument'),	fact('is metal'),					fact('has three sides').
+instrument(cow_bell)			:- fact('is a percussion instrument'),	fact('is metal'),					fact('has animal in name'),							fact('Christopher Walken needs more'),
+instrument(xylophone) 			:- fact('is a percussion instrument'),	fact('is metal'),					fact('has multiple sized bars').
+instrument(bells) 	 			:- fact('is a percussion instrument'),	fact('is metal'),					fact('has multiple sized tubes').
+instrument(wood_block) 			:- fact('is a percussion instrument'),	fact('is made entirely from wood').
+instrument(drum_kit) 			:- fact('is a percussion instrument'),	fact('is a set or kit'),			fact('has multiple percussion instruments'),.
 % percussion(tenor_drum) .
 % percussion(tam_tam) .
 % percussion(glockenspiel) .
@@ -56,50 +56,50 @@ instrument(drum_kit) 			:- fact('is percussion'),	fact('is a set or kit'),			fac
 % percussion(tubular_bells) .
 % percussion(mark_tree) .
 
-instrument(piano) 			:- fact('is a keyboard'),		fact('has strings'),	fact('mallets strike strings to play').
-instrument(harpsicord) 		:- fact('is a keyboard'),		fact('hss strings'),	fact('plucks strings to play').
-instrument(pipe_organ) 		:- fact('is a keyboard'),		fact('has pipes').
-instrument(accordion) 		:- fact('is a keyboard'),		fact('has bellows'),	fact('is pulled and pressed to play').
+instrument(piano) 			:- fact('is a keyboard instrument'),		fact('has strings'),	fact('mallets strike strings to play').
+instrument(harpsicord) 		:- fact('is a keyboard instrument'),		fact('hss strings'),	fact('plucks strings to play').
+instrument(pipe_organ) 		:- fact('is a keyboard instrument'),		fact('has pipes').
+instrument(accordion) 		:- fact('is a keyboard instrument'),		fact('has bellows'),	fact('is pulled and pressed to play').
 % keyboard(celesta) .
 
-strings(violin) .
-strings(first_violin) .
-strings(second_violin) .
-strings(viola) .
-strings(cello) .
-strings(bass) .
-strings(upright_bass) .
-strings(double_bass) .
-strings(harp) .
-strings(guitar) .
-strings(classical_guitar) .
+instrument(violin) 			:- fact('is a stringed instrument'),		fact('played with a bow'),	fact('is played from shoulder'),	fact('is smaller than viola').
+instrument(viola) 			:- fact('is a stringed instrument'),		fact('played with a bow'),	fact('is played from shoulder').
+instrument(bass) 				:- fact('is a stringed instrument'),		fact('played with a bow'),	fact('is played upright'),	fact('is larger than cello'),.
+instrument(cello) 				:- fact('is a stringed instrument'),		fact('played with a bow'),	fact('is played upright').
+instrument(guitar) 			:- fact('is a stringed instrument'),		fact('is plucked of strummed'),		fact('has a finger/fret board'),.
+instrument(harp) 				:- fact('is a stringed instrument'),		fact('is plucked or strummed').
+% strings(first_violin) .
+% strings(second_violin) .
+% strings(upright_bass) .
+% strings(double_bass) .
+% strings(classical_guitar) .
 
-other(theremin) .
-other(ondes_martenot) .
-other(electric_guitar) .
-other(electric_bass) .
-other(hammond_organ) .
-other(lowrey_organ) .
-other(ring_modulators) .
-other(synthesizer) .
-other(novachord) .
+% other(theremin) .
+% other(ondes_martenot) .
+% other(electric_guitar) .
+% other(electric_bass) .
+% other(hammond_organ) .
+% other(lowrey_organ) .
+% other(ring_modulators) .
+% other(synthesizer) .
+% other(novachord) .
 
-play(pluck) .
-play(blow) .
-play(strum) .
-play(key) .
-play(strike) .
-play(bow) .
-play(drag) .
-play(hit) .
-play(wave) .
+% play(pluck) .
+% play(blow) .
+% play(strum) .
+% play(key) .
+% play(strike) .
+% play(bow) .
+% play(drag) .
+% play(hit) .
+% play(wave) .
 
-part(key) .
-part(string) .
-part(bridge) .
-part(mouth_piece) .
-part(reed) .
-part(hole) .
+% part(key) .
+% part(string) .
+% part(bridge) .
+% part(mouth_piece) .
+% part(reed) .
+% part(hole) .
 
 is_true(Question) :-
   (
